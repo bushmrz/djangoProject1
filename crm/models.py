61aef3707489d47cf66c32f4b80ddf60cf271577
bushmrzzz <buuushmrz@mail.ru>
2022-07-19 01:14:25 +0300

    Add superuser and first page

--- crm/models.py
+++ crm/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
-# Create your models here.
 class Order(models.Model):
-    order_dt = models.DateTimeField(auto_now=True)
-    order_name = models.CharField(max_length=100)
-    order_phone = models.CharField(max_length=10)
+    order_dt = models.DateTimeField(auto_now=True, verbose_name= 'Дата')
+    order_name = models.CharField(max_length=100, verbose_name= 'Имя')
+    order_phone = models.CharField(max_length=10, verbose_name= 'Номер телефона')
+
+    def __str__(self):
+        return  self.order_name
+
+    class Meta:
+        verbose_name = 'заказ'
+        verbose_name_plural = 'Заказы'
