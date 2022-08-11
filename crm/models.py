b8d098bff0e8374358dbc50f050952a36e40b91a
bushmrzzz <buuushmrz@mail.ru>
2022-08-12 01:16:53 +0300

    added admin panel for orders editor and few news things

--- crm/models.py
+++ crm/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+class StatusCrm(models.Model):
+    status_name = models.CharField(max_length=20, verbose_name='Название статуса')
+
+    def __str__(self):
+        return self.status_name
+
+    class Meta:
+        verbose_name = 'статус'
+        verbose_name_plural = 'Статусы'
+
 class Order(models.Model):
     order_dt = models.DateTimeField(auto_now=True, verbose_name= 'Дата')
     order_name = models.CharField(max_length=100, verbose_name= 'Имя')
     order_phone = models.CharField(max_length=10, verbose_name= 'Номер телефона')
+    order_status = models.ForeignKey(StatusCrm, on_delete=models.PROTECT,
+                                     null = True, blank=True, verbose_name='Статус')
 
     def __str__(self):
         return  self.order_name
@@ -11,3 +23,15 @@ class Order(models.Model):
     class Meta:
         verbose_name = 'заказ'
         verbose_name_plural = 'Заказы'
+
+class ComentCrm(models.Model):
+    comment_binding = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Заявка')
+    comment_text = models.TextField(verbose_name='Текст комментария')
+    comment_dt = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.comment_text
+
+    class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'Комментарии'
