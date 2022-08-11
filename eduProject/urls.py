61d1819fa7feda6813eb9bc2e01fe52a95cc3315
bushmrzzz <buuushmrz@mail.ru>
2022-08-11 20:32:33 +0300

    create new admin panel for sliders editor

--- eduProject/urls.py
+++ eduProject/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from crm import views
 
@@ -21,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.first_page),
     path('thanks_page/', views.thanks_page, name='thanks_page')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
